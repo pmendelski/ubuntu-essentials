8e7a9283e152fec16aae601b8d59d153b7f552c5
mendlik <mendelski.pawel@gmail.com>
2016-03-13 16:06:48 +0100

    Added pygmentize and some funny and handy utils

--- 01-development/02-utils.sh
+++ 01-development/02-utils.sh
@@ -9,3 +9,19 @@ package "jq"
 
 # Geeky editor
 package	"vim"
+
+# Pygmentize
+package	"python-pygments"
+
+# Pure fun
+# Quote of the day
+package "fortune"
+# Funny creatures
+# fortune | cowsay -f $(ls /usr/share/cowsay/cows/ | shuf -n1)
+# for f in `cowsay -l | tac | head -n -1 | tac | tr ' ' '\n' | sort`; do
+#     cowsay -f $f "Hello from $f"
+# done
+package "cowsay"
+# Ascii art
+# figlet -f slant <Some Text>
+package "figlet"
