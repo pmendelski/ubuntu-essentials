744e7f2b3692adaff9703b03011227f7d7f7570c
mendlik <mendelski.pawel@gmail.com>
2016-03-16 16:36:52 +0100

    Added logitech pairing comment

--- 00-system/personalization.sh
+++ 00-system/personalization.sh
@@ -15,3 +15,8 @@ package "alarm-clock-applet"
 
 # redshift
 package "redshift-gtk"
+
+# Logitech unifying receiver
+# http://www.omgubuntu.co.uk/2013/12/logitech-unifying-receiver-linux-solaar
+# repository "ppa:daniel.pavel/solaar"
+# package "solaar"
